1c9cc0700f5d19614d97bca29fc5b6ddb7486ca6
Amon RY <121221742+amonry@users.noreply.github.com>
2023-05-27 15:37:49 +0300

    Update web3.js

--- src/config/web3.js
+++ src/config/web3.js
@@ -1,11 +1,24 @@
 import Web3 from 'web3'
 import { InjectedConnector } from '@web3-react/injected-connector'
 
+//chainlist.org
+//exclusive support for high-demand blockchain networks
 
 const connector = new InjectedConnector({ supportedChainIds: [
   1,
+  
   4,
-  56
+  
+  56, //Binance
+  
+  137, //Bolygon
+  
+  42161, //Arbitrum
+  
+  43114, //Avalancha
+  
+  1284, //Moonbeam
+  
   ]
 })
 
